09264942c83c0ece8c3ed69267995d10f3143dbf
nehamanoj1105 <143329337+nehamanoj1105@users.noreply.github.com>
2024-09-02 10:48:07 +0530

    Create diamond_pattern.c

---
+++ task-03/SubTask3/diamond_pattern.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() 
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) 
+    {
+        printf("%*s", n - i - 1, "");
+        for (int j = 0; j < 2 * i + 1; j++) 
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for (int i = n - 2; i >= 0; i--) 
+    {
+        printf("%*s", n - i - 1, "");
+        for (int j = 0; j < 2 * i + 1; j++) 
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
